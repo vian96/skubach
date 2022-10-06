ecf7f13a3deaca8c9e6ee32914cd4b343640101b
vian96 <51120611+vian96@users.noreply.github.com>
2022-10-06 17:28:05 +0300

    Added all flags cp, a bit of refactoring for ls

--- argc/my_ls.cpp
+++ argc/my_ls.cpp
@@ -21,12 +21,7 @@ void print_file (dirent *dnt, bool flag_a, bool flag_l, const char *path);
 void print_permissions (struct stat struct_buf);
 
 #define DEB(...)
-
 // #define DEB(...) printf(__VA_ARGS__)
-
-
-// char *path_buffer = (char*) calloc(PATH_MAX, sizeof (char));
-// int dir_end = 0;
 
 int main (int argc, char **argv) {
     const struct option long_option[] =
@@ -56,17 +51,11 @@ int main (int argc, char **argv) {
         }
     }
     
-    
     const char *f_name = NULL;
     if (argc >= optind + 1)
         f_name = argv[optind];
     else
         f_name = ".";
-
-    // strcpy (path_buffer, f_name);
-    // dir_end = strlen (path_buffer);
-    // path_buffer[dir_end] = '/';
-    // dir_end++;
 
     print_dir (f_name, flag_a, flag_l, flag_R);
 }
@@ -108,8 +97,6 @@ void print_dir (const char *f_name, bool flag_a, bool flag_l, bool flag_R) {
 
     if (flag_R)
         while (directories.size()) {
-            // printf("\n");
-
             dirent *entry = directories.back();
             directories.pop_back();
 
@@ -118,44 +105,11 @@ void print_dir (const char *f_name, bool flag_a, bool flag_l, bool flag_R) {
             printf ("\n\n%s:\n", file_path);
 
             print_dir (file_path, flag_a, flag_l, flag_R);
-            // printf ("\n");
         }
 
     
     DEB ("\nended while\n");
-    // printf ("\n");
     closedir (dir);
-
-    /* 
-    if (flag_R) {
-        DEB ("trying opendir %s\n", f_name);
-        dir = opendir (f_name);
-        DEB ("trying readdir\n");
-        dnt = readdir (dir);
-
-        while (dnt) {
-            if (!flag_a && dnt->d_name[0] == '.')
-                continue;
-
-            strcat (path_buffer, dnt->d_name);
-            // printf ("path: %s\n", path_buffer);
-            struct stat struct_buf = {};
-            stat (path_buffer, &struct_buf);
-
-            if ((S_ISDIR (struct_buf.st_mode))) {
-                DEB ("Found dir\n");
-                printf ("%s\n", path_buffer);
-                print_dir (path_buffer, flag_a, flag_l, flag_R);
-            }
-
-            dnt = readdir (dir);
-            path_buffer[dir_end] = 0;
-        }
-        
-        printf ("\n");
-        closedir (dir);
-    }
-    // */
 }
 
 void print_file (dirent *dnt, bool flag_a, bool flag_l, const char *path) {
@@ -169,8 +123,6 @@ void print_file (dirent *dnt, bool flag_a, bool flag_l, const char *path) {
     char file_path[PATH_MAX] = {};
     sprintf (file_path, "%s/%s", path, dnt->d_name);
 
-    // strcat (path_buffer, dnt->d_name);
-    // printf ("path: %s\n", path_buffer);
     struct stat struct_buf = {};
     stat(file_path, &struct_buf);
 
@@ -183,8 +135,6 @@ void print_file (dirent *dnt, bool flag_a, bool flag_l, const char *path) {
     const char *time_str = ctime (&struct_buf.st_ctime) + 4; // +4 is to skip weekday name
     printf ("\t%.*s", strlen (time_str) - 1, time_str);
     printf (" %s\n", dnt->d_name);
-
-    // path_buffer[dir_end] = 0;
 }
 
 void print_permissions (struct stat struct_buf) {
--- file_cp/my_cp.cpp
+++ file_cp/my_cp.cpp
@@ -1,19 +1,128 @@
+#include <getopt.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <grp.h>
+#include <pwd.h>
+#include <unistd.h>
+#include <utime.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include <errno.h>
 
 int main (int argc, char **argv) {
-    if (argc < 2) {
-        printf ("ERROR: Not enough args!\n");
+    const struct option long_option[] =
+    {
+        {"help", no_argument, NULL, 'h'},
+        {"verbose", no_argument, NULL, 'v'}, 
+        {"interactive", no_argument, NULL, 'i'},
+        {"force", no_argument, NULL, 'f'},
+        {"preserve", no_argument, NULL, 'p'},
+        {NULL, no_argument, NULL, 0}
+    };
+	int long_index = 0;
+    int opt = 0;
+    const char short_option[] = "hvifp";
+    
+    bool flag_h = 0, flag_v = 0, flag_i = 0, flag_f = 0, flag_p = 0;
+    
+    while((opt = getopt_long(argc, argv, short_option, long_option, &long_index)) != -1)
+    {
+        switch(opt)
+        {
+        case 'h': flag_h = 1;
+            break;
+        case 'v': flag_v = 1;
+            break;
+        case 'i': flag_i = 1;
+            break;
+        case 'f': flag_f = 1;
+            break;
+        case 'p': flag_p = 1;
+            break;
+        case '?': 
+            printf ("Unknown parameter: -%c\n", optopt);
+        }
+    }
+    
+    if (flag_h) {
+        printf ("This is hello for the program!\n"
+                "I am too lazy to write it\n"
+                "So let us both pretend help is there\n");
         return 0;
     }
-    FILE *f_from = fopen (argv[1], "r");
-    FILE *f_to   = fopen (argv[2], "w");
+
+    if (argc < optind + 2) {
+        printf ("ERROR: not enough file names!\n");
+        return 0;
+    }
+
+    const char *from_name = argv[optind], *to_name = argv[optind + 1];
+
+    if (flag_i && access(to_name, F_OK) == 0) { // file exists
+        printf ("Are you sure you want to rewrite %s?\n", to_name);
+        const int ANSWER_SIZE = 128;
+        char answer[ANSWER_SIZE] = {};
+
+        fgets (answer, ANSWER_SIZE, stdin);
+        if (toupper (answer[0]) != 'Y') {
+            printf ("Fine, i will not copy\n");
+            return 0;
+        }
+        printf ("Ok, I will copy\n");
+    }
+
+    FILE *f_to = fopen (to_name, "w");
+    if (f_to == NULL) { // failed to open
+        if (errno == EACCES) {
+            if (flag_f) {
+                remove (to_name);
+                f_to = fopen (to_name, "w");
+            }
+            else {
+                printf ("ERROR: no access to written file\n");
+                return 0;
+            }
+        }
+
+        if (errno != EACCES) { // another if because error may occur at second open
+            printf ("ERROR opening written file, errno is %d\n", errno);
+            return 0;
+        }
+    }
+
+    FILE *f_from = fopen (from_name, "r");
+    if (f_from == NULL) {
+        printf ("ERROR opening read file, errno is %d\n", errno);
+        return 0;
+    }
 
     const int BUF_SIZE = 64;
     char *buf = (char*) calloc (BUF_SIZE, sizeof (char));
     while (fgets (buf, BUF_SIZE, f_from) != NULL)
         fputs (buf, f_to);
 
+    if (flag_p) {
+        struct stat struct_buf = {};
+        stat(from_name, &struct_buf);
+
+        chmod (to_name, struct_buf.st_mode);
+        chown (to_name, struct_buf.st_uid, struct_buf.st_gid);
+
+        utimbuf timbuf = {
+            .actime = struct_buf.st_atim.tv_sec, 
+            .modtime = struct_buf.st_mtim.tv_sec
+        };
+        utime (to_name, &timbuf);
+    }
+
     fclose (f_from);
     fclose (f_to);
+
+    if (flag_v)
+        printf ("%s > %s\n", from_name, to_name);
 }
